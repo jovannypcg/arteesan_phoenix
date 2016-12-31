ef79d518a4aea904c1acdd242001f78b24706a7d
Jovanny Cruz <jovannypcg@yahoo.com>
2016-12-30 22:18:51 -0600

    refactor(users): Remove unnecessary fields from table users in migration file

--- priv/repo/migrations/20161231035229_users.exs
+++ priv/repo/migrations/20161231035229_users.exs
@@ -4,13 +4,10 @@ defmodule Arteesan.Repo.Migrations.Users do
   def change do
     create table(:users) do
       add :first_name,  :string
-      add :last_name,   :string
-      add :birthdate,   :naive_datetime
-      add :picture,     :string, default: ""
       add :email,       :string
       add :password,    :string
-      add :status,      :string, default: "ACTIVE"
       add :role,        :string, default: "CUSTOMER"
+      add :active,      :boolean, default: true
 
       timestamps()
     end
