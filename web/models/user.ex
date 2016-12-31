b38c2396f2f703505176f4f4b68ae8b52da6a700
Jovanny Cruz <jovannypcg@yahoo.com>
2016-12-30 22:28:16 -0600

    feat(users): Add changeset function to model

--- web/models/user.ex
+++ web/models/user.ex
@@ -8,4 +8,12 @@ defmodule Arteesan.User do
     field :role,        :string
     field :active,      :boolean
   end
+
+  def changeset(data, params \\ %{}) do
+    data
+    |> cast(params, [:first_name, :email, :password, :role, :active])
+    |> validate_required([:first_name, :email, :password])
+    |> validate_format(:email, ~r/(\w+)@([\w.]+)/)
+    |> unique_constraint(:email)
+  end
 end
