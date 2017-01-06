2b5e717bf3e769aec30da2c729ab89fcab61c935
Jovanny Cruz <jovannypcg@yahoo.com>
2017-01-06 13:05:36 -0600

    chore(users): Rename helper function to generate signup changeset

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,18 +4,19 @@ defmodule Arteesan.UserController do
   alias Arteesan.User
 
   def new(conn, _params) do
-    changeset = User.changeset(%User{}, %{})
+    changeset = User.signup_changeset(%User{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"user" => user}) do
-    changeset = User.changeset(%User{}, user)
+    changeset = User.signup_changeset(%User{}, user)
 
     case Repo.insert(changeset) do
       { :ok, saved_user } ->
         conn
         |> put_flash(:info, "Your account was created")
+        |> put_session(:user_id, saved_user.id)
         |> redirect(to: "/")
       { :error, err_changeset } ->
         render conn, "new.html", changeset: err_changeset
